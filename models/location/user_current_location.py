f304c5025c1d36997731680dc8ae5ac79252502f
Sereth1 <papakias444@gmail.com>
2025-04-29 18:19:09 +0700

    AA

--- models/location/user_current_location.py
+++ models/location/user_current_location.py
@@ -1,15 +1,15 @@
-from sqlalchemy import Column, String, DateTime, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, DateTime, ForeignKey
 from database import Base
 from datetime import datetime
-from typing import Optional
 
 class UserCurrentLocation(Base):
     __tablename__ = "user_current_location"
 
-    user_id = Column(String, ForeignKey("user.id"), primary_key=True)
-    ip = Column(String)
-    country = Column(String)
-    city = Column(String)
-    region = Column(String)
-    timezone: Optional[str]
-    updated_at = Column(DateTime, default=datetime.utcnow)
+    user_id: Mapped[str] = mapped_column(String, ForeignKey("user.id"), primary_key=True)
+    ip: Mapped[str] = mapped_column(String)
+    country: Mapped[str] = mapped_column(String)
+    city: Mapped[str] = mapped_column(String)
+    region: Mapped[str] = mapped_column(String)
+    timezone: Mapped[str] = mapped_column(String)
+    updated_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
