0f8a247a5e98c622951df15e186fae11afa6269f
Sereth1 <papakias444@gmail.com>
2025-05-02 16:24:04 +0700

    gh

--- schemas/location/user_location_logs.py
+++ schemas/location/user_location_logs.py
@@ -3,17 +3,19 @@ from typing import Optional
 from datetime import datetime
 from uuid import UUID  # ✅ this!
 
+# schemas/location/user_location_logs.py
 class UserLocationLog(BaseModel):
-    id: UUID  # ✅ Fix here
+    id: Optional[UUID] = None
     user_id: str
     ip: str
     country: str
     city: str
     region: str
     timezone: Optional[str]
-    created_at: Optional[datetime]
-    updated_at: Optional[datetime]
+    created_at: Optional[datetime] = None
+    updated_at: Optional[datetime] = None
 
     model_config = {
         "from_attributes": True
     }
+
